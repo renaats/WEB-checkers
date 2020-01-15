562be339543091b5732521a81628174870dd22b7
renaats <renaatsj@gmail.com>
2020-01-15 16:43:34 +0100

    added routing to splash and game screens

--- checkers/app.js
+++ checkers/app.js
@@ -9,6 +9,15 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
+var express = require("express");
+var http = require("http");
+
+var port = process.argv[2];
+var app = express();
+
+app.use(express.static(__dirname + "/public"));
+http.createServer(app).listen(port);
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
--- checkers/routes/index.js
+++ checkers/routes/index.js
@@ -2,8 +2,16 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
+// router.get('/', function(req, res, next) {
+//   res.render('index', { title: 'Express' });
+// });
+
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.sendFile("splash.html", {root: "./public"});
+});
+
+router.get("/play", function(req, res, next) {
+  res.sendFile("game.html", {root: "./public"});
 });
 
 module.exports = router;
